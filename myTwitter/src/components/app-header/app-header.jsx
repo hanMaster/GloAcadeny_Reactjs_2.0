715cea0c8b14cac0ed9b0bffce07c75031c4d5c4
hanMaster <w54661c@gmail.com>
2019-06-14 21:56:11 +1000

    Задание 6 усложненное

--- myTwitter/src/components/app-header/app-header.jsx
+++ myTwitter/src/components/app-header/app-header.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import './app-header.css';
+import React from "react";
+import "./app-header.css";
 
 const AppHeader = ({ allPosts, likedPosts }) => {
+  let liked = likedPosts % 10 === 1 && likedPosts !== 11 ? "понравилась" : "понравилось";
+  let record = "записей";
+  let count = allPosts;
+  count = count > 20 ? allPosts % 10 : count;
+  switch (count) {
+    case 1:
+      record = "запись";
+      break;
+    case 2:
+    case 3:
+    case 4:
+      record = "записи";
+      break;
+    default:
+      record = "записей";
+  }
+
   return (
     <div className="app-header d-flex">
       <h1>Andrey Khalepov</h1>
-      <h2>
-        {allPosts} записей, из них понравилось {likedPosts}
-      </h2>
+      <h2>{`${allPosts} ${record}, из них ${liked} ${likedPosts}`}</h2>
     </div>
   );
 };
--- myTwitter/src/components/app/app.jsx
+++ myTwitter/src/components/app/app.jsx
@@ -96,7 +96,7 @@ export default class App extends Component {
   searchPosts = (items, term) => {
     if (term.length === 0) return items;
     return items.filter(item => {
-      return item.label.indexOf(term) > -1;
+      return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
     });
   };
 
